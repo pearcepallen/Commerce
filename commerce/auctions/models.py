be9c308b44da7a4ab43fb45e7f84cf38edae7b95
Pearce Allen <pearce.p.allen@gmail.com>
2021-01-03 21:58:57 -0500

    Added models and create page

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,17 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    name = models.CharField(max_length = 50)
+    start_bid = models.IntegerField()
+    description = models.CharField(max_length = 100)
+    image_link = models.CharField(max_length = 2000)
+    def __str__(self):
+        return f"{self.id}: Name:{self.name}, Bid:{self.start_bid}, Desc:{self.description}"
+
+class Bid(models.Model):
+    bid = models.IntegerField()
+
+class Comment(models.Model):
+    comment = models.CharField(max_length = 250)
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -61,3 +61,9 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create(request):
+    if request.method == "POST":
+        pass
+    return render(request, "auctions/create.html")
