f1a70a43a8b68942e2f1c96a8b9e04a08d6eecd5
Pearce Allen <pearce.p.allen@gmail.com>
2021-01-11 22:48:35 -0500

    Finalized django admin interface

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -3,9 +3,7 @@ from .models import *
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Category)
 admin.site.register(Listing)
 admin.site.register(Bid)
-admin.site.register(Watchlist)
-admin.site.register(Comment)
+
 
